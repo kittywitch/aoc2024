a76949b305851d333cabb1d194d1a4a33d65dbfe
Kat Inskip <kat@inskip.me>
2024-12-03 16:35:05 -0800

    feat(day1): part 2 solved

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn day_1() {
     let mut list_2 = Vec::new(); 
 
     for line in file_reader.lines() {
-        let words: Vec<i32> = line.unwrap().split_whitespace().map(|word| i32::from_str(word).unwrap()).collect();
+        let words: Vec<u32> = line.unwrap().split_whitespace().map(|word| u32::from_str(word).unwrap()).collect();
         list_1.push(words[0]);
         list_2.push(words[1]);
     }
@@ -23,14 +23,25 @@ fn day_1() {
     list_1.sort();
     list_2.sort();
 
+    let mut uniques = list_1.clone();
+    uniques.dedup();
     let zipped_list = list_1.iter().zip(list_2.iter());
-    
-    let mut total: u32 = 0;
+
+    // Part 1 
+    let mut total_diff: u32 = 0;
     for (a, b) in zipped_list {
-        total += a.abs_diff(*b);
-    
+        total_diff += a.abs_diff(*b); 
     }
-    println!("{}", total);
+    println!("{}", total_diff);
+
+    // Part 2
+    let mut total_similarity: u32 = 0;
+    for unique in uniques {
+        let occurences: u32 = list_2.iter().filter(|&item| *item == unique).count().try_into().unwrap();
+        total_similarity += unique * occurences
+    }
+
+    println!("{}", total_similarity);
 }
 
 fn main() {
