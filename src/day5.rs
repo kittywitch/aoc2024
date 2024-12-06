31b70a5717a9f624872c40c1713c5152c23587a7
Kat Inskip <kat@inskip.me>
2024-12-06 01:04:32 -0800

    feat(day5): part 2

--- src/day5.rs
+++ src/day5.rs
@@ -2,6 +2,7 @@ use std::io::{BufReader, BufRead};
 use std::fs::File;
 use std::str::FromStr;
 use std::collections::{HashSet,HashMap};
+use std::cmp::Ordering;
 
 pub fn splitter(line: String, split_char: &str) -> Vec<String> {
     line.split(split_char).map(String::from).collect()
@@ -39,13 +40,13 @@ pub fn day_5(file_reader: BufReader<File>) -> (u32, u32) {
 
     let mut incorrect_updates: Vec<Vec<u32>> = Vec::new();
     for update in &updates {
-        for (i, page) in update.clone().into_iter().enumerate() {
+        'outer: for (i, page) in update.clone().into_iter().enumerate() {
             let prior_pages = &update[0..i];
             for prior_page in prior_pages {
                 if rules.contains_key(&prior_page) {
                     if rules[prior_page].contains(&page) {
                         incorrect_updates.push(update.clone());
-                        break
+                        break 'outer; 
                     }
                 }
             }
@@ -61,7 +62,32 @@ pub fn day_5(file_reader: BufReader<File>) -> (u32, u32) {
         middles.push(update[middle_index]);
     }
 
-    //dbg!(&order_rules, &unordered_update_pages);
+    let mut corrected_updates = Vec::new();
+
+    for update in incorrect_updates {
+        let mut corrected_update = update.clone();
+        for (i, page) in update.clone().into_iter().enumerate() {
+            corrected_update.sort_by(|&a, &b|
+                if rules.contains_key(&a) && rules[&a].contains(&b) {
+                    Ordering::Less
+                } else if rules.contains_key(&b) && rules[&b].contains(&a) {
+                    Ordering::Greater
+                } else {
+                    Ordering::Equal
+                }
+            );
+        }
+        corrected_updates.push(corrected_update);
+    }
     
-    return (middles.into_iter().sum::<u32>(), 0)
+    let mut corrected_middles = Vec::new();
+    for update in corrected_updates {
+        let length = update.len();
+        let middle_index = length / 2;
+        corrected_middles.push(update[middle_index]);
+    }
+
+    dbg!(&corrected_middles);
+    
+    return (middles.into_iter().sum::<u32>(), corrected_middles.into_iter().sum::<u32>())
 }
