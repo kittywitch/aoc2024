97e2855a9ebc61736ec67ecde9e4da8513945f2c
Kat Inskip <kat@inskip.me>
2024-12-09 17:26:06 -0800

    feat(day 7): part 2

--- src/day7.rs
+++ src/day7.rs
@@ -9,7 +9,8 @@ enum Operations {
     Multiply,
     Divide,
     Add,
-    Subtract
+    Subtract,
+    Concatenate,
 }
 
 pub fn day_7(file_reader: BufReader<File>) -> (usize, usize) {
@@ -28,25 +29,21 @@ pub fn day_7(file_reader: BufReader<File>) -> (usize, usize) {
             Operations::Add => return operand_1 + operand_2,
             Operations::Divide => return operand_1 / operand_2,
             Operations::Subtract => return operand_1 - operand_2,
+            Operations::Concatenate => return format!("{}{}", operand_1.to_string(), operand_2.to_string()).parse().unwrap(),
         }
     }
 
-    fn evaluate_if_true(res: usize, builds: Vec<usize>) -> Option<usize> {
-        let allowed_operations = vec![
-            Operations::Multiply,
-            Operations::Add,
-        ].into_iter();
-
+    fn evaluate_if_true(allowed_operations: &Vec<Operations>, res: usize, builds: Vec<usize>) -> Option<usize> {
         let positions_between_items = builds.len() - 1;
     
-        let permutations: Vec<Vec<Operations>> = repeat_n(allowed_operations, positions_between_items)
+        let permutations: Vec<Vec<&Operations>> = repeat_n(allowed_operations.into_iter(), positions_between_items)
             .multi_cartesian_product()
             .collect();
 
         for permutation in permutations {
             let remains = &builds[1..];
             let potential_result: usize = remains.into_iter().enumerate().fold(builds[0], |acc, (idx, i)| 
-                do_operation(permutation[idx], acc, *i)
+                do_operation(*permutation[idx], acc, *i)
             );
 
             if res == potential_result {
@@ -56,12 +53,30 @@ pub fn day_7(file_reader: BufReader<File>) -> (usize, usize) {
         return None
     }
 
+    let part_1_operations = vec![
+        Operations::Multiply,
+        Operations::Add,
+    ];
+
     let mut true_results = Vec::new();
-    for result in results {
-        if let Some(true_result) = evaluate_if_true(result.0, result.1) {
+    for result in &results {
+        if let Some(true_result) = evaluate_if_true(&part_1_operations, result.0, result.1.clone()) {
             true_results.push(true_result);
         }
     }
+    
+    let part_2_operations = vec![
+        Operations::Multiply,
+        Operations::Add,
+        Operations::Concatenate,
+    ];
 
-    return (true_results.into_iter().sum(), 0)
+    let mut true_results_2 = Vec::new();
+    for result in results {
+        if let Some(true_result) = evaluate_if_true(&part_2_operations, result.0, result.1) {
+            true_results_2.push(true_result);
+        }
+    }
+
+    return (true_results.into_iter().sum(), true_results_2.into_iter().sum())
 }
